f307746a12a43aecc6e7bba859c956167875d8e5
13684045763 <315862477@qq.com>
2016-12-05 18:35:28 +0800

    Update GateMonitor.js

--- Gate/js/GateMonitor.js
+++ Gate/js/GateMonitor.js
@@ -2,21 +2,15 @@ $(function () {
     $('#maincontent h2').click(function () {
         $(this).next().slideToggle();
     });
-
-
-
     var inputs = $('#maincontent input');
     inputs.each(function (index) {
         var ID = $(this).attr('value');
         $(this).change(function () {
             if ($(this).is(':checked')) {
-                $('#maincontent').find('[id="' + ID + '"]').show(500);
+                $('#maincontent').find('[id="' + ID + '"]').fadeIn(500);
             } else {
-                $('#maincontent').find('[id="' + ID + '"]').hide(500);
+                $('#maincontent').find('[id="' + ID + '"]').fadeOut(500);
             }
         });
     });
-
-
-
 });
