6ba5ca54a5c8c7394db737e73c4bbf6937133b3c
13684045763 <315862477@qq.com>
2016-12-07 08:51:07 +0800

    Add files via upload

--- Gate/js/GateMonitor.js
+++ Gate/js/GateMonitor.js
@@ -1,8 +1,5 @@
 $(function () {
-    /*绑定鼠标点击h2事件*/
-    $('#maincontent h2').click(function () {
-        $(this).next().stop().slideToggle();
-    });
+
     /*绑定鼠标离开事件*/
     $('#maincontent .bgcolor').hover(function () {
         $(this).find('.choise').stop().slideDown();
@@ -11,28 +8,47 @@ $(function () {
     });
     /*input控制li显示消失事件,用each事件给每个input添加表单改变change()函数*/
     var inputs = $('#maincontent input');
-    inputs.each(function (index) {
+    var labels = $('#maincontent label');
+    /*给label checked true添加样式*/
+    labels.css({
+        'background-color': 'darkblue',
+        'box-shadow': '1px 1px 3px gray',
+        'color': 'white'
+    });
+
+    inputs.change(function () {
         var ID = $(this).attr('value');
-        $(this).change(function () {
-            if ($(this).is(':checked')) {
-                var ele = $('#maincontent').find('[id="' + ID + '"]');
-                ele.removeClass('none').addClass('block'); //fadeIn(500);
-            } else {
-                $('#maincontent').find('[id="' + ID + '"]').removeClass('block').addClass('none'); //fadeOut(500);
-            }
-        });
+        var ele = $('#maincontent').find('[id="' + ID + '"]');
+        if ($(this).is(':checked')) {
+            ele.removeClass('none').addClass('block');
+            /*checked true*/
+            $(this).parent().css({
+                'background-color': 'darkblue',
+                'box-shadow': '1px 1px 3px gray',
+                'color': 'white'
+            });
+        } else {
+            ele.removeClass('block').addClass('none');
+            /*checked false*/
+            $(this).parent().css({
+                'background-color': 'transparent',
+                'box-shadow': 'none',
+                'color': 'black'
+            });
+        }
     });
 
     var n = 0;
     var ele = $('#maincontent li');
     var eleLength = ele.length;
-    var t =setInterval(Do,4000);
 
-    Do();
+    //定义定时器让动画函数每4秒运行一次
+    var t = setInterval(Do, 5000);
+    var eleli;
 
+    //动画函数
     function Do() {
-        var eleli = $('#maincontent').find('.block');
-        //alert(eleli.length);
+        eleli = $('#maincontent').find('.block'); //将显示的li放入eleli集合中
 
         if (n < eleli.length) {
             eleli.eq(n).stop().delay(1000).animate({
@@ -44,11 +60,9 @@ $(function () {
                 'opacity': '0'
             }, 0).css('opacity', '1');
             n++;
-        }else{
-            n=0;
-            //Do();
+        } else {
+            n = 0;
+            Do();
         }
     }
 });
-
-
